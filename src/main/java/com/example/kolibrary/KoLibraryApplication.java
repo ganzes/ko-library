c5c5f12c620bc3916489da98883e13e5f8daca48
ganzes <charczuk.arkadiusz@gmail.com>
2019-11-21 18:54:16 +0100

    initital commit

---
+++ src/main/java/com/example/kolibrary/KoLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.example.kolibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KoLibraryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kolibrary/KoLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kolibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoLibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
