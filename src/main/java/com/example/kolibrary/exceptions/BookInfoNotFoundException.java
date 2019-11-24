c5b3a0421c1fa79c2f1011cd99370844a3cffd69
ganzes <charczuk.arkadiusz@gmail.com>
2019-11-24 18:06:45 +0100

    added exceptions

---
+++ src/main/java/com/example/kolibrary/exceptions/BookInfoNotFoundException.java
@@ -0,0 +1,9 @@
+package com.example.kolibrary.exceptions;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND,reason = "Not found")
+public class BookInfoNotFoundException extends RuntimeException {
+}
---
+++ src/main/java/com/example/kolibrary/exceptions/BookRentInfoException.java
@@ -0,0 +1,8 @@
+package com.example.kolibrary.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND,reason = "Not found")
+public class BookRentInfoException extends RuntimeException {
+}
